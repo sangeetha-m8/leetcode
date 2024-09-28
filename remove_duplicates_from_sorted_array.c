3db6ccae8d7fa1c9b8ccf90c47ef4de9c0fe58b5
Sangeetha Mariappan <psrenc08@gmail.com>
2024-09-28 14:08:40 +0100

    Create remove_duplicates_from_sorted_array.c
    26. Remove Duplicates from Sorted Array Leetcode Problem

---
+++ remove_duplicates_from_sorted_array.c
@@ -0,0 +1,10 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int count = 1;
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] != nums[i-1]) {
+            nums[count] = nums[i];
+            count++;
+        }
+    }
+    return count;
+}
